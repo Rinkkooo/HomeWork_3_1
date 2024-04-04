15a2fc98ebb18076fc527730faadc85928a0e241
rinkkooo <bekturtelmanov04@gmail.com>
2024-04-04 16:32:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_3_1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.homework_3_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView edTxt;
+    Button sendBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        edTxt = findViewById(R.id.editText);
+        sendBtn = findViewById(R.id.sendBtn);
+
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("RESULT_TEXT", edTxt.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
